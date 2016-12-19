1e5012f81c47035127a4fbb9684d1864c197965d
Gorkem1991 <gorkem_agac@hotmail.com>
2016-12-19 23:08:53 +0000

    Climbzone

---
+++ Climbzone/OpeningHours.php
@@ -0,0 +1,110 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+	  
+<head>
+	<meta charset="utf-8">
+	<title>Climbzone</title>
+	<link rel="stylesheet" type="text/css" href="mycss.css">
+	
+<link rel="shortcut icon"   href="logoicon2.ico">
+</head>
+
+<body>
+		<div class="background1">	
+			<div class="inner1">
+				<a href="OpeningHours.htm">
+					<img class="logo" alt="logo" src="Images/logo4.png" width="45" height="45">
+				</a>
+					<h1 class="headerh1">Climbzone</h1>
+			</div>	
+		</div>
+
+		<div class="background2">
+			<div class="inner2">
+				<a class="Home" href= "princomp.htm">Home</a><br>
+				<hr>
+				<a class="BirthdayClimbingParties" href= "BirthdayCP.htm">Birthday Climbing Parties</a><br>
+				<hr>
+				<a class="HenStagClimbingParties" href= "HSClimbingP.htm">Hen/Stag Climbing Parties</a><br>
+				<hr>
+				<a class="ProfessionalClimbingCoaching" href= "ProfessionalC.htm">Professional Climbing Coaching</a><br>
+				<hr>
+				<a class="ClimbingProducts" href= "ClimbingP.htm">Climbing Products</a><br>
+				<hr>
+				<a class="Cafe" href= "Cafe.htm">Climbzone Cafe</a><br>
+				<hr>
+				<a class="OpeningHours" href= "OpeningHours.htm" style="background-color:#dca53b">Opening Hours</a><br>
+				<hr>
+				<a class="ContactUs" href= "ContactUs.htm">Contact Us</a><br>
+			</div>
+		</div>
+		
+		<div class="background3">
+			<div class="inner3">
+
+<h1 class="Openingh">Opening Hours</h1>
+<table>
+
+
+<thead>
+<tr>
+    <td><strong>Day</strong></td>
+    <td><strong>Open</strong></td>
+    <td><strong>Close</strong></td>
+	
+</tr>
+</thead>
+<tbody>
+		<tr>
+		    <td>Monday</td>
+		    <td rowspan="4">07:00</td>		
+		    <td rowspan="3">22:00</td>
+		  </tr>
+		  <tr>
+		    <td>Tuesday</td>
+		    
+		    
+		  </tr>
+		  <tr>
+		    <td>Wednesday</td>
+		   	
+		    
+		  </tr>
+		  <tr>
+		    <td>Thursday</td>
+		  	
+		    <td>23:00</td>
+		  </tr>
+		  <tr>
+		    <td>Friday</td>
+		    <td rowspan="2">06:30</td>		
+		    <td rowspan="2">02:00</td>
+		  </tr>
+		  <tr>
+		    <td>Saturday</td>
+		  
+		    
+		  </tr>
+		   <tr>
+		    <td>Sunday</td>
+		    <td>09:00</td>		
+		    <td>21:00</td>
+		  </tr>
+</tbody>
+</table>
+				
+			</div>
+		</div>
+		
+		<div class="background4">
+			<div class="inner4">
+				<small class ="copy" > Copyright &copy; 2014</small>
+			</div>
+		</div>
+			
+</body>
+
+
+</html>
---
+++ Climbzone/dbinfo.php
@@ -0,0 +1,5 @@
+<?PHP
+$username="u792385924_root";
+$password="123456";
+$database="u792385924_hours";
+?> 
